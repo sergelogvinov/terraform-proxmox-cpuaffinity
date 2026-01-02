e560094185e8d80564d26f6d214ffbe5f5ce4249
Serge Logvinov <serge.logvinov@sinextra.dev>
2026-01-02 13:00:48 +0700

    fix: negative order with numa

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ locals {
     var.shift >= 0 ? flatten([flatten([for r in range(length(v) / 2) : [v[r], v[r + length(v) / 2]]])]) : reverse(flatten([flatten([for r in range(length(v) / 2) : [v[r], v[r + length(v) / 2]]])]))
   ]
 
-  shift = var.shift >= 0 ? var.shift * length(try(local.cpus[0], [])) : abs(length(var.cpu_affinity) + var.shift) * length(try(local.cpus[0], []))
+  shift = var.shift >= 0 ? var.shift * length(try(local.cpus[0], [])) : abs(length(var.cpu_affinity) + var.shift + 1) * length(try(local.cpus[0], [])) - var.vms * var.cpus
 
   vm_arch = { for k in flatten([
     for inx in range(var.vms) : {
       inx : var.shift >= 0 ? inx : var.vms - inx - 1
-      cpus : var.shift >= 0 ? slice(flatten(local.cpus), inx * var.cpus + local.shift, (inx + 1) * var.cpus + local.shift) : slice(flatten(local.cpus), (inx - 1) * var.cpus + local.shift, inx * var.cpus + local.shift)
+      cpus : slice(flatten(local.cpus), inx * var.cpus + local.shift, (inx + 1) * var.cpus + local.shift)
       shift : local.shift
-      numa : var.shift >= 0 ? { for numa in range(length(var.cpu_affinity)) : numa => setintersection(local.cpus[numa], slice(flatten(local.cpus), inx * var.cpus + local.shift, (inx + 1) * var.cpus + local.shift)) } : { for numa in range(length(var.cpu_affinity)) : numa => setintersection(local.cpus[numa], slice(flatten(local.cpus), (inx - 1) * var.cpus + local.shift, inx * var.cpus + local.shift)) }
+      numa : { for numa in range(length(var.cpu_affinity)) : numa => setintersection(local.cpus[numa], slice(flatten(local.cpus), inx * var.cpus + local.shift, (inx + 1) * var.cpus + local.shift)) }
     }
   ]) : k.inx => k }
 }
